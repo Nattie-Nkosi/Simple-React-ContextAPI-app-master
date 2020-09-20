684c80390cb5610b24bd8f3158fd6491cd6de779
Nutty Nkosi <nkosin361@gmail.com>
2020-09-20 22:36:15 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './App.css';
+
+import MadodaList from './MadodaList';
+import AddIndodaForm from './AddIndoda';
+
+import {AmadodaContextProvider} from './AmadodaContext';
+
+function App() {
+
+  return (
+    <AmadodaContextProvider>
+    <div className="App"> 
+      <h1>Add indoda</h1>
+      <AddIndodaForm />
+      <MadodaList />
+    </div>
+    </AmadodaContextProvider>
+  );
+}
+
+export default App;
